ecde03a2436b733f8040f83406fa4758d84dfa5a
Felipa Mendez <felipalouise@gmail.com>
2022-02-25 10:23:02 -0600

    commit tree 0b79aa04794a90d8e611e73bd31b6b17fbffc381

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr Chocolate', 3, 'M', 'brown', 'mild', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'F', 'black', 'testy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 1, 'M', 'black and white', 'cudly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'M', 'black and white', 'cudly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'black', 'spicy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 20, 'M', 'green', 'mean', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'purple', 'aloof', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, 'F', 'camo', 'cool', 1);
